e7b2987ced0103c40efc494064f1a25a3e022ac2
Justin Garner <j.garner828@gmail.com>
2022-08-14 10:58:24 -0400

    changed endpoint for home route to / instead of /home

--- src/main/java/com/garnerju/auctionhouse/controller/HomeController.java
+++ src/main/java/com/garnerju/auctionhouse/controller/HomeController.java
@@ -2,12 +2,12 @@ package com.garnerju.auctionhouse.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String getHome() {
         return "home";
     }
